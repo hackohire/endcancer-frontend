fd9bdfe557fdeb5fac2c0200a911a545883f909c
Sumit Vekariya <sumitvekariya7@gmail.com>
2019-09-16 13:00:24 +0530

    Endcancer Backend Prod URL Changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   COGNITO_AUTH_DOMAIN: 'https://endcancer.auth.us-east-1.amazoncognito.com',
   platform_graphql_url: 'https://qs3c3z6r40.execute-api.ap-south-1.amazonaws.com/prod/graphql',
-  graphql_url: 'https://vqjblbhbbg.execute-api.ap-south-1.amazonaws.com/dev/graphql',
+  graphql_url: 'https://fpl89vj3kg.execute-api.ap-south-1.amazonaws.com/prod/graphql',
   endcancerFilesBucket: 'https://endcancer-files-prod.s3.amazonaws.com/public/',
   applicationId: '5d2ef428bbfa6576b357d5d4',
   oauth: {
